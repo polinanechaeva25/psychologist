3fbe1f158f28f93ac54c8df2c6c12d0db0128340
Polina Nechaeva <polina2000_21@mail.ru>
2023-01-12 17:54:52 +0300

    a bit styles

--- app/mainapp/views.py
+++ app/mainapp/views.py
@@ -3,6 +3,20 @@ from django.shortcuts import render
 from django.views.generic import ListView
 
 
-class MainListView(ListView):
+class TitleContextMixin:
+
+    def get_title(self):
+        return getattr(self, 'title', '')
+
+    def get_context_data(self, **kwargs):
+        context = super(TitleContextMixin, self).get_context_data(**kwargs)
+        context.update(
+            title=self.get_title()
+        )
+        return context
+
+
+class MainListView(TitleContextMixin, ListView):
+    title = 'Psychologist from Miami'
     model = User
     template_name = 'mainapp/index.html'
